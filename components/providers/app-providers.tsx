b8c5898e318c5bbd3ff7f21f1992584b95c5b677
Harris Joseph <harris@accountscope.app>
2026-03-29 00:18:27 +0000

    CRITICAL FIX: Remove all offline features (indexedDB blocking deployments)

--- components/providers/app-providers.tsx
+++ components/providers/app-providers.tsx
@@ -2,7 +2,6 @@
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
-import { OfflineProvider } from "@/components/providers/offline-provider";
 
 interface Props {
   children: React.ReactNode;
@@ -12,7 +11,7 @@ export const AppProviders = ({ children }: Props) => {
   return (
     <ThemeProvider>
       <QueryProvider>
-        <OfflineProvider>{children}</OfflineProvider>
+        {children}
       </QueryProvider>
     </ThemeProvider>
   );
